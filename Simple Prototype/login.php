f699fe9fe840dc8f9296612bf9e691cf17c0abe9
Matt Pryor <mpryor@siue.edu>
2013-09-28 15:39:06 -0500

    I added the login stuff to the page.
    It's not complete, but when the user first accesses the site they will
    now be presented with a login page.  After logging in, they will no
    longer see the original login page.

--- Simple Prototype/login.php
+++ Simple Prototype/login.php
@@ -1,132 +1,41 @@
-<?php
-session_start();
-
-ob_start();
-
-$rpx_api_key = "6f7db617b7e06dd9497f089bee3d72648e03bbcd";
-
-$engage_pro = false;
-
-$token = $_POST['token'];
-
-if(strlen($token) == 40) {
-  $post_data = array('token'  => $token,
-                     'apiKey' => $rpx_api_key,
-                     'format' => 'json',
-                     'extended' => 'false');
-
-  $curl = curl_init();
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($curl, CURLOPT_URL, 'https://rpxnow.com/api/v2/auth_info');
-  curl_setopt($curl, CURLOPT_POST, true);
-  curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
-  curl_setopt($curl, CURLOPT_HEADER, false);
-  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($curl, CURLOPT_FAILONERROR, true);
-  $result = curl_exec($curl);
-  if ($result == false){
-    echo "\n".'Curl error: ' . curl_error($curl);
-    echo "\n".'HTTP code: ' . curl_errno($curl);
-    echo "\n"; var_dump($post_data);
-  }
-  curl_close($curl);
-
-  $auth_info = json_decode($result, true);
-
-  if ($auth_info['stat'] == 'ok') {
-    $profile = $auth_info[profile];
-    $email = $profile[email];
-    $identifier = $profile[identifier];
-$identifier = trim($identifier,"https://www.google.com/profiles/");
-    $_SESSION['name'] = $identifier;
-    $displayname = $profile[displayname];
-
-
-
-$con=mysqli_connect("localhost","root","htam91","users");
-$result = mysqli_query($con,"SELECT * FROM customers
-WHERE accountid = '$identifier'");
-if($result->num_rows > 0)
-{
-}
-else
-{
-mysqli_query($con,"INSERT INTO customers (accountid, email, accesslvl, testprogress, testscore, trainingprogress)
-VALUES ('$identifier','$email',0, 0,0,0)");
-}
-
-mysqli_close($con);
-
-    } else {
-      echo "\n".'An error occured: ' . $auth_info['err']['msg']."\n";
-      var_dump($auth_info);
-      echo "\n";
-      var_dump($result);
-    }
-}else{
-  echo 'Authentication canceled.';
-}
-$debug_out = ob_get_contents();
-ob_end_clean();
+<?php session_start();
+include ('loginscript.php');
 ?>
-
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
 <head>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-	<link href="css/common.css" rel="stylesheet" type="text/css">
-	<link href='http://fonts.googleapis.com/css?family=Black+Ops+One' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="css/dropdown.css" type="text/css" />
-	<script type="text/javascript" src="js/dropdown.js"></script>
-	<script type="text/javascript" src="js/electricslide.js"></script>
+<link rel="stylesheet" href="css/common.css" type="text/css" />
 </head>
-<body>
-    <div id="wrapper">
-        <div id="heads">
-            <div id="headerTitle">
-                STEM
-            </div>
-            <div id="headerText">
-                Regional Science and Engineering Challenge
-            </div>
-            <div id="headerSubText">
-                Training and Testing
-            </div>
-            <div id="whiteline" style="height:2px; `background-color: white; left:50%; width:950px; margin-left:-475px; top:100px; position: absolute;">
-        </div>
 
-    </div>
-    <div id="tabspace"></div>
 
-   <div>
-        <?php if($_SESSION['name'] == "") : ?>
-            <a class="janrainEngage" href="#">Login</a></li>
-        <?php else : ?>
-            <a href ="logout.php">Logout</a>
-        <?php endif; ?>
 
-   </div>
+<script type="text/javascript">
+    (function() {
+        if (typeof window.janrain !== 'object') window.janrain = {};
+        if (typeof window.janrain.settings !== 'object') window.janrain.settings = {};
+        
+        janrain.settings.tokenUrl = 'http://www.judgestraining.org/index.php';
 
-    <!--<div id="midline" style = "height:100%; width:1px; background-color:pink; position:absolute; left:50%;"></div>-->
-    <img id="logo" src="images/logo.png"/>
-    <div id="testfont">
-        <h1> </h1>
-    </div>
+        function isReady() { janrain.ready = true; };
+        if (document.addEventListener) {
+          document.addEventListener("DOMContentLoaded", isReady, false);
+        } else {
+          window.attachEvent('onload', isReady);
+        }
 
-    <script src="js/login.js"></script>
-    <script type="text/javascript">
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-1332079-8']);
-        _gaq.push(['_trackPageview']);
-
-        (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
-
-    </script>
-</body>
-</html>
-
+        var e = document.createElement('script');
+        e.type = 'text/javascript';
+        e.id = 'janrainAuthWidget';
+        if (document.location.protocol === 'https:') {
+          e.src = 'https://rpxnow.com/js/lib/stem-login/engage.js?minify=false&3719813926';
+        } else {
+          e.src = 'http://rpxnow.com/js/lib/stem-login/engage.js?minify=false&3719813926';
+        }
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(e, s);
+    })();
+</script>
+<div id ="blue bar" style="width:100%; position: absolute; top: 50%; height: 435; background-color: rgb(0,10,255); margin: -214px 0 0 -0;">
+</div>
+<div id ="content" style="position: relative; left:50%; top:50%; margin: -212px 0 0 -475px">
+    <div id="janrainEngageEmbed" style="position: relative; top:50%; left:50%; margin: -78px 0 0 -190px; width: 380px;"></div>
+</div>
+</div>
---
+++ Simple Prototype/loginscript.php
@@ -0,0 +1,114 @@
+<?php session_start();
+/*
+ * This script is intended as an educational tool.
+ * Please look at the PHP SDK if you are looking for somthing suited to a new project.
+ * https://github.com/janrain/Janrain-Sample-Code/tree/master/php/janrain-engage-php-sdk
+ */
+
+ob_start();
+/*
+ Below is a very simple and verbose PHP 5 script that implements the Engage token URL processing and some popular Pro/Enterprise examples.
+ The code below assumes you have the CURL HTTP fetching library with SSL.  
+*/
+
+// PATH_TO_API_KEY_FILE should contain a path to a plain text file containing only
+// your API key. This file should exist in a path that can be read by your web server,
+// but not publicly accessible to the Internet.
+$rpx_api_key = "6f7db617b7e06dd9497f089bee3d72648e03bbcd";
+
+/*
+ Set this to true if your application is Pro or Enterprise.
+ Set this to false if your application is Basic or Plus.
+*/
+$engage_pro = false;
+
+/* STEP 1: Extract token POST parameter */
+$token = $_POST['token'];
+
+//Some output to help debugging
+/*
+echo "SERVER VARIABLES:\n";
+var_dump($_SERVER);
+echo "HTTP POST ARRAY:\n";
+var_dump($_POST);
+*/
+
+if(strlen($token) == 40) {//test the length of the token; it should be 40 characters
+
+  /* STEP 2: Use the token to make the auth_info API call */
+  $post_data = array('token'  => $token,
+                     'apiKey' => $rpx_api_key,
+                     'format' => 'json',
+                     'extended' => 'false'); //Extended is not available to Basic.
+
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_URL, 'https://rpxnow.com/api/v2/auth_info');
+  curl_setopt($curl, CURLOPT_POST, true);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
+  curl_setopt($curl, CURLOPT_HEADER, false);
+  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($curl, CURLOPT_FAILONERROR, true);
+  $result = curl_exec($curl);
+  if ($result == false){
+    echo "\n".'Curl error: ' . curl_error($curl);
+    echo "\n".'HTTP code: ' . curl_errno($curl);
+    echo "\n"; var_dump($post_data);
+  }
+  curl_close($curl);
+
+
+  /* STEP 3: Parse the JSON auth_info response */
+  $auth_info = json_decode($result, true);
+
+  if ($auth_info['stat'] == 'ok') {
+  /*
+    echo "\n auth_info:";
+    echo "\n"; var_dump($auth_info);
+  */
+    $profile = $auth_info[profile];
+    $email = $profile[email];
+    $identifier = $profile[identifier];
+    $identifier = trim($identifier,"https://www.google.com/profiles/");
+    $displayname = $profile[displayname];
+    // DATABASE STUFF GOES HERE ===============================
+  // Create connection
+  $con=mysqli_connect("steminfo.db.10915569.hostedresource.com","steminfo","Outreach4!","steminfo");
+  // Check connection
+  if (mysqli_connect_errno($con))
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  mysqli_query($con,"INSERT IGNORE INTO users (accountid, email, accesslevel,testprogress,testscore,trainingprogress) VALUES ($identifier,'$email',0,0,0,0)");
+  $result = mysqli_query($con,"SELECT * FROM users WHERE email = '$email' ");
+  $row = mysqli_fetch_array($result);
+  
+  
+  $_SESSION['identifier'] = $identifier;
+  $_SESSION['email'] = $row['email'];
+  $_SESSION['accesslevel'] = $row['accesslevel'];
+  $_SESSION['testprogress'] = $row['testprogress'];
+  $_SESSION['testscore'] = $row['testscore'];
+  $_SESSION['trainingprogress'] = $row['trainingprogress'];
+  
+  
+
+
+ mysqli_close($con);
+    // END DATABASE STUFF =====================================
+    
+
+    } else {
+      // Gracefully handle auth_info error.  Hook this into your native error handling system.
+      echo "\n".'An error occured: ' . $auth_info['err']['msg']."\n";
+      var_dump($auth_info);
+      echo "\n";
+      var_dump($result);
+    }
+}else{
+  // Gracefully handle the missing or malformed token.  Hook this into your native error handling system.
+  echo 'Authentication canceled.';
+}
+$debug_out = ob_get_contents();
+ob_end_clean();
+?>
